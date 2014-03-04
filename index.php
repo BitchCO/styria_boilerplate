c3bf1618d0e91aabb25687dc4ebe02c80730a146
Dario Knežević <dario.knezevic@styria.hr>
2014-03-04 13:10:16 +0100

    [SB] - Proba

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]><html class="no-js lt-ie9"><![endif]-->
+<!--[if gt IE 8]><!--><html class="no-js"><!--<![endif]-->
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title>Title</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width">     
+    <link rel="stylesheet" href="css/plugins.css">
+    <link rel="stylesheet" href="css/grid.css">
+    <link rel="stylesheet" href="css/globals.css">
+    <script src="js/libs/modernizr-2.7.0.min.js"></script>    
+</head>
+<body>
+
+    <header>
+        <div class="container">
+            <div class="grid-4">
+
+            </div>
+            <div class="grid-8 parent">
+
+                
+            </div>
+        </div>
+    </header>
+
+
+
+
+    <footer>
+
+    </footer>
+
+
+	<!-- ODKOMENTIRAT ZA PRODUKCIJU <script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>-->
+	<script>window.jQuery || document.write('<script src="js/libs/jquery-2.0.3.min.js"><\/script>')</script>
+	<!-- ODKOMENTIRAT AKO SE KORISTI <script src="http://cdnjs.cloudflare.com/ajax/libs/gsap/1.10.3/TweenMax.min.js"></script> -->
+
+	<!--[if lt IE 9]>
+		<script src="js/libs/jquery-1.10.2.min.js"><\/script>
+	<![endif]-->
+
+	<script src="js/plugins.js"></script>
+	<script src="js/main.js"></script>
+</body>
+</html>
